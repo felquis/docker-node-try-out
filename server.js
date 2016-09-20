28875882cedacd9169f1da5019d22c49df3006d8
Felquis Gimenes <frgformenton@gmail.com>
2016-09-19 21:39:57 -0300

    Examples from nodejs.org
      https://nodejs.org/en/docs/guides/nodejs-docker-webapp/

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const PORT = 8080
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World\n')
+})
+
+app.listen(PORT)
+console.log('Running on localhost:8080')
